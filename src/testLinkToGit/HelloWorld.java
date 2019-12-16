a93c7cde50b4b2b4f7c6bf7cfc1c6f71cf3b6c4a
Doug Shanks <djshanks@comcast.net>
2019-12-16 00:27:26 -0800

    new java project using git

---
+++ src/testLinkToGit/HelloWorld.java
@@ -0,0 +1,20 @@
+package testLinkToGit;
+/**
+ * 
+ */
+
+/**
+ * @author djshanks
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
